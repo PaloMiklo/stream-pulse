c439dede6df2b1c8eba3f72d2c3dacb14831605d
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2024-11-01 19:52:07 +0100

    Solve disconnection error

--- core/src/main/java/com/palomiklo/streampulse/context/AsynchronousContext.java
+++ core/src/main/java/com/palomiklo/streampulse/context/AsynchronousContext.java
@@ -4,11 +4,12 @@ import jakarta.servlet.AsyncContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public record AsynchronousContext(HttpServletRequest request, AsyncContext asyncContext) {
-    public static AsynchronousContext startAsyncContext(HttpServletRequest request, HttpServletResponse response) {
-        request.setAttribute("org.apache.catalina.ASYNC_SUPPORTED", true);
-        AsyncContext asyncContext = request.startAsync(request, response);
-        asyncContext.setTimeout(0);
-        return new AsynchronousContext(request, asyncContext);
+public record AsynchronousContext(HttpServletRequest req, AsyncContext actx) {
+
+    public static AsynchronousContext startAsyncContext(HttpServletRequest req, HttpServletResponse res) {
+        req.setAttribute("org.apache.catalina.ASYNC_SUPPORTED", true);
+        AsyncContext actx = req.startAsync(req, res);
+        actx.setTimeout(0);
+        return new AsynchronousContext(req, actx);
     }
 }
---
+++ core/src/main/java/com/palomiklo/streampulse/listener/Listener.java
@@ -0,0 +1,45 @@
+package com.palomiklo.streampulse.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.palomiklo.streampulse.connection.ConnectionInfo;
+
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.AsyncEvent;
+import jakarta.servlet.AsyncListener;
+
+public class Listener {
+
+    private static final Logger log = LoggerFactory.getLogger(Listener.class);
+
+    public static void addListeners(AsyncContext actx, ConnectionInfo streamPulse) {
+        actx.addListener(new AsyncListener() {
+            @Override
+            public void onComplete(AsyncEvent event) {
+                log.debug("Async process completed for connection ID: {}", streamPulse.id());
+                if (streamPulse.connection().isConnected()) {
+                    streamPulse.connection().closeConnection();
+                }
+            }
+
+            @Override
+            public void onTimeout(AsyncEvent event) {
+                log.debug("Async process timed out for connection ID: {}", streamPulse.id());
+                streamPulse.connection().closeConnection();
+            }
+
+            @Override
+            public void onError(AsyncEvent event) {
+                log.error("Error occurred for connection ID: {}: {}", streamPulse.id(), event.getThrowable().getLocalizedMessage());
+                streamPulse.connection().closeConnection();
+            }
+
+            @Override
+            public void onStartAsync(AsyncEvent event) {
+                log.debug("Async process started for connection ID: {}", streamPulse.id());
+            }
+        });
+    }
+
+}
