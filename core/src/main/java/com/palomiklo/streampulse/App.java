1553b4c7ea12ff0a91bc0aeddcf1f1df7ef0e9f8
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2024-11-01 19:11:28 +0100

    Remove preview features

--- core/src/main/java/com/palomiklo/streampulse/App.java
+++ core/src/main/java/com/palomiklo/streampulse/App.java
@@ -1,5 +1,7 @@
 package com.palomiklo.streampulse;
 
 public class App {
-    public static void main(String[] args) {}
+
+    public static void main(String... args) {
+    }
 }
--- spring/src/main/java/com/palomiklo/streampulse/App.java
+++ spring/src/main/java/com/palomiklo/streampulse/App.java
@@ -1,7 +1,8 @@
 package com.palomiklo.streampulse;
 
 public class App {
-    public static final void main(String[] args) {
+
+    public static final void main(String... args) {
         System.out.println("Hello World!");
     }
 }
