d78bb0eb7f8dcbd3a2b056dbba4e9ee7e5c87619
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2024-11-01 19:52:07 +0100

    Solve disconnection error

--- core/src/main/java/com/palomiklo/streampulse/connection/ConnectionInfo.java
+++ core/src/main/java/com/palomiklo/streampulse/connection/ConnectionInfo.java
@@ -3,12 +3,14 @@ package com.palomiklo.streampulse.connection;
 import java.util.UUID;
 
 import com.palomiklo.streampulse.config.ConfigLoader;
+import com.palomiklo.streampulse.context.AsynchronousContext;
 
-public record ConnectionInfo(UUID id, Connection connection) {
+public record ConnectionInfo(UUID id, Connection connection, AsynchronousContext ctx) {
 
-    public ConnectionInfo(UUID id, Connection connection) {
+    public ConnectionInfo(UUID id, Connection connection, AsynchronousContext ctx) {
         ConfigLoader.initialize();
         this.id = id;
         this.connection = connection;
+        this.ctx = ctx;
     }
 }
