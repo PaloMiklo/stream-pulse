1868807cdd4fc75f44bdd026464f14c80e240da8
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2024-11-03 19:54:22 +0100

    Extend utils by new wraps

--- core/src/main/java/com/palomiklo/streampulse/util/Wrap.java
+++ core/src/main/java/com/palomiklo/streampulse/util/Wrap.java
@@ -1,9 +1,13 @@
 package com.palomiklo.streampulse.util;
 
+import java.util.function.Supplier;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.palomiklo.streampulse.connection.Connection;
+
+import jakarta.annotation.Nullable;
 
 public class Wrap {
 
@@ -22,7 +26,43 @@ public class Wrap {
             runnable.run();
         } catch (Exception e) {
             log.error(message, e);
+            fallback.run();
         }
     }
 
+    @FunctionalInterface
+    public interface CheckedSupplier<T> {
+
+        T get() throws Exception;
+    }
+
+    @Nullable
+    public static <T> T wrap(CheckedSupplier<T> supplier, String message, Runnable fallback) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            log.error(message, e);
+            fallback.run();
+            return null;
+        }
+    }
+
+    @Nullable
+    public static <T> T wrap(CheckedSupplier<T> supplier, String message) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            log.error(message, e);
+            return null;
+        }
+    }
+
+    public static <T> T wrap(CheckedSupplier<T> supplier, String message, Supplier<T> fallback) {
+        try {
+            return supplier.get();
+        } catch (Exception e) {
+            log.error(message, e);
+            return fallback.get();
+        }
+    }
 }
