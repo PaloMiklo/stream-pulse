0f4e5569494fd5300ce54b3ad38e90491464bccc
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2024-11-04 19:06:10 +0100

    Convenient way to change versions

---
+++ version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <new_version>"
+  exit 1
+fi
+
+NEW_VERSION=$1
+
+FILES=$(find . -type f \( -name "pom.xml" -o -name "pack.sh" -o -name "run.sh" \))
+
+for FILE in $FILES; do
+  if [[ $FILE == *.xml ]]; then
+    sed -i "/<groupId>com.palomiklo<\/groupId>/{n;n;s|<version>.*</version>|<version>$NEW_VERSION</version>|}" "$FILE"
+    echo "Updated version in $FILE"
+  elif [[ $FILE == *.sh ]]; then
+    sed -i "s|VERSION=\"[^\"]*\"|VERSION=\"$NEW_VERSION\"|g" "$FILE"
+    echo "Updated version in $FILE"
+  fi
+done
+
+echo "😈  Version updated to $NEW_VERSION in all relevant files."
